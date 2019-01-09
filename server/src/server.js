0a3a9b46456dc91fedb8291b3df4838e8b9ebb24
Jan-Marius <janmariv@stud.ntnu.no>
2019-01-09 10:24:21 +0100

    Første endpunkt på plass

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@ import path from 'path';
 import reload from 'reload';
 import fs from 'fs';
 import { Students } from './models.js';
+import { Region } from './models.js';
 
 type Request = express$Request;
 type Response = express$Response;
@@ -15,6 +16,55 @@ let app = express();
 
 app.use(express.static(public_path));
 app.use(express.json()); // For parsing application/json
+
+app.get('/api/regions', (req: Request, res: Response) => {
+  return Region.findAll().then(regions => res.send(regions));
+});
+
+app.post('/api/regions', (req: Request, res: Response) => {
+  if (
+    !req.body ||
+    typeof req.body.name != 'string' ||
+    typeof req.body.lat != 'number' ||
+    typeof req.body.lon != 'number' ||
+    typeof req.body.county_id != 'number'
+  )
+    return res.sendStatus(400);
+  return Region.create({
+    name: req.body.name,
+    lat: req.body.lat,
+    lon: req.body.lon,
+    county_id: req.body.county_id
+  }).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)));
+});
+
+app.get('/api/regions/:region_id', (req: Request, res: Response) => {
+  return Region.findOne({ where: { id: Number(req.params.id) } }).then(
+    region => (region ? res.send(region) : res.sendStatus(404))
+  );
+});
+
+app.put('/api/regions/:region_id', (req: Request, res: Response) => {
+  if (
+    !req.body ||
+    typeof req.body.region_id != 'number' ||
+    typeof req.body.name != 'string' ||
+    typeof req.body.lat != 'number' ||
+    typeof req.body.lon != 'number' ||
+    typeof req.body.county_id != 'number'
+  )
+    return res.sendStatus(400);
+
+  return Region.update(
+    {
+      name: req.body.name,
+      lat: req.body.lat,
+      lon: req.body.lon,
+      county_id: req.body.county_id
+    },
+    { where: { region_id: Number(req.params.region_id) } }
+  ).then(count => (count ? res.sendStatus(200) : res.sendStatus(404)));
+});
 
 app.get('/students', (req: Request, res: Response) => {
   return Students.findAll().then(students => res.send(students));
