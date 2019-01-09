920f8bce112f2da9412d92c000e6c096ae858a5f
Marius Nygård <mariunyg@stud.ntnu.no>
2019-01-09 14:29:25 +0100

    added placholder footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,26 @@
 // @flow
 import * as React from 'react';
 import { Component } from 'react-simplified';
+import { Minside } from './Minside'
+import { Footer } from './Footer'
 
 import '../styles/styles.css';
 import '../styles/loginmodal.css';
 import '../styles/registermodal.css';
 import '../styles/grid-list-toggle.css';
+import '../styles/simple-sidebar.css';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
-            <div style={{fontSize: 25}}>
+            <div style={{ fontSize: 16 }}>
                 This is the app, place all the initialization of other components here.
+            <Minside ></Minside>
+                <Footer></Footer>
             </div>
+
         );
     }
 }
