5fd8179c528dd3f7f751661718849a0a3e9256a8
Marius Nygård <mariunyg@stud.ntnu.no>
2019-01-09 08:27:46 +0100

    cleanup forntend

--- client/src/index.js
+++ client/src/index.js
@@ -3,182 +3,31 @@
 import ReactDOM from 'react-dom';
 import * as React from 'react';
 import { Component } from 'react-simplified';
-import { HashRouter, Route, NavLink } from 'react-router-dom';
-import { Alert } from './widgets';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 import { studentService } from './services';
+// import { Home } from './Home'
+// import { Navbar } from './Navbar'
 
-// Reload application when not in production environment
-if (process.env.NODE_ENV !== 'production') {
-  let script = document.createElement('script');
-  script.src = '/reload/reload.js';
-  if (document.body) document.body.appendChild(script);
-}
-
-import createHashHistory from 'history/createHashHistory';
-const history = createHashHistory(); // Use history.push(...) to programmatically change path, for instance after successfully saving a student
-
-class Menu extends Component {
-  render() {
-    return (
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <NavLink activeStyle={{ color: 'darkblue' }} exact to="/">
-                React example
-              </NavLink>
-            </td>
-            <td>
-              <NavLink activeStyle={{ color: 'darkblue' }} to="/students">
-                Students
-              </NavLink>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
-}
+const root = document.getElementById('root');
 
 class Home extends Component {
   render() {
-    return <div>React example with component state</div>;
+    return <div>dette er hovedsiden</div>;
   }
 }
 
-class StudentList extends Component {
-  students = [];
-
+class Navbar extends Component {
   render() {
-    return (
-      <ul>
-        {this.students.map(student => (
-          <li key={student.email}>
-            <NavLink activeStyle={{ color: 'darkblue' }} exact to={'/students/' + student.id}>
-              {student.firstName} {student.lastName}
-            </NavLink>{' '}
-            <NavLink activeStyle={{ color: 'darkblue' }} to={'/students/' + student.id + '/edit'}>
-              edit
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-
-  mounted() {
-    studentService
-      .getStudents()
-      .then(students => (this.students = students))
-      .catch((error: Error) => Alert.danger(error.message));
+    return <div>detter er en navdr</div>;
   }
 }
 
-class StudentDetails extends Component<{ match: { params: { id: number } } }> {
-  student = null;
-
-  render() {
-    if (!this.student) return null;
-
-    return (
-      <div>
-        <ul>
-          <li>First name: {this.student.firstName}</li>
-          <li>Last name: {this.student.lastName}</li>
-          <li>Email: {this.student.email}</li>
-        </ul>
-      </div>
-    );
-  }
-
-  mounted() {
-    studentService
-      .getStudent(this.props.match.params.id)
-      .then(student => (this.student = student))
-      .catch((error: Error) => Alert.danger(error.message));
-  }
-}
-
-class StudentEdit extends Component<{ match: { params: { id: number } } }> {
-  student = null;
-
-  render() {
-    if (!this.student) return null;
-
-    return (
-      <form>
-        <ul>
-          <li>
-            First name:{' '}
-            <input
-              type="text"
-              value={this.student.firstName}
-              onChange={(event: SyntheticInputEvent<HTMLInputElement>) => {
-                if (this.student) this.student.firstName = event.target.value;
-              }}
-            />
-          </li>
-          <li>
-            Last name:{' '}
-            <input
-              type="text"
-              value={this.student.lastName}
-              onChange={(event: SyntheticInputEvent<HTMLInputElement>) => {
-                if (this.student) this.student.lastName = event.target.value;
-              }}
-            />
-          </li>
-          <li>
-            Email:{' '}
-            <input
-              type="text"
-              value={this.student.email}
-              onChange={(event: SyntheticInputEvent<HTMLInputElement>) => {
-                if (this.student) this.student.email = event.target.value;
-              }}
-            />
-          </li>
-        </ul>
-        <button type="button" onClick={this.save}>
-          Save
-        </button>
-      </form>
-    );
-  }
-
-  mounted() {
-    studentService
-      .getStudent(this.props.match.params.id)
-      .then(student => (this.student = student))
-      .catch((error: Error) => Alert.danger(error.message));
-  }
-
-  save() {
-    if (!this.student) return null;
-
-    studentService
-      .updateStudent(this.student)
-      .then(() => {
-        let studentList = StudentList.instance();
-        if (studentList) studentList.mounted(); // Update Studentlist-component
-        if (this.student) history.push('/students/' + this.student.id);
-      })
-      .catch((error: Error) => Alert.danger(error.message));
-  }
-}
-
-const root = document.getElementById('root');
-if (root)
-  ReactDOM.render(
-    <HashRouter>
-      <div>
-        <Alert />
-        <Menu />
-        <Route exact path="/" component={Home} />
-        <Route path="/students" component={StudentList} />
-        <Route exact path="/students/:id" component={StudentDetails} />
-        <Route exact path="/students/:id/edit" component={StudentEdit} />
-      </div>
-    </HashRouter>,
-    root
-  );
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+    </div>
+  </BrowserRouter>,
+  root
+);
