a542ffd795491609b73ea452024a5e8435611708
Jan-Marius <janmariv@stud.ntnu.no>
2019-01-10 13:33:37 +0100

    Endpoints: County Post and Delete2

--- server/src/server.js
+++ server/src/server.js
@@ -481,7 +481,6 @@ app.delete('/api/categories/:category_id', (req: Request, res: Response) => {
   }).then(category => (category ? res.send() : res.status(500).send()));
 });
 
-
 // Hot reload application when not in production environment
 if (process.env.NODE_ENV !== 'production') {
   let reloadServer = reload(app);
