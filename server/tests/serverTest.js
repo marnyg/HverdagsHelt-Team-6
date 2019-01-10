6a85f373ef2ac3d23a131ccd7067799432f65164
ingelig <ingelig@stud.ntnu.no>
2019-01-10 14:29:01 +0100

    Gjort klart for å skrive tester på endepunkter

--- server/tests/serverTest.js
+++ server/tests/serverTest.js
@@ -1,21 +1,10 @@
 //
-const request = require('supertest');
-const server = require('../src/models.js');
-
-beforeAll(async () => {
-    // do something before anything else runs
-    console.log('Testing endpoints');
-});
-/*
-// close the server after each test
-afterAll(() => {
-    server.close();
-    console.log('server closed!');
-});*/
+import { application } from '../src/server';
+import request from 'supertest';
 
 describe('/api/cases', () => {
     test('It should response the GET method', (done) => {
-        request(server).get('/api/cases').then((response) => {
+        request(application).get('/api/cases').then((response) => {
             expect(response.statusCode).toBe(200);
             done();
         });
