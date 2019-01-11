ec7227a277f01c87a334c038781aa80560d341e8
Håvard Ramberg <haavarhr@stud.ntnu.no>
2019-01-11 12:37:23 +0100

    FinalForm

--- client/src/components/NewCase.js
+++ client/src/components/NewCase.js
@@ -1,13 +1,15 @@
 import ReactDOM from 'react-dom';
 import * as React from 'react';
 import { Component } from 'react-simplified';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import { countyService } from '../services/CountyService';
 import { regionService } from '../services/RegionService';
 import { categoryService } from '../services/CategoryService';
+import { caseService } from '../services/CaseService';
 import { Notify } from './Notify';
 import LocationService from '../services/LocationService';
 import GoogleApiWrapper from './GoogleApiWrapper';
+import Case from '../classes/Case';
 
 class NewCase extends Component {
   form = null;
@@ -19,7 +21,8 @@ class NewCase extends Component {
   list2 = null;
   lastResortAddress = null;
   lastResortAddressLabel = null;
-  pos = { lat: 59.9138688, lon: 10.752245399999993 }; // Last resort position OSLO
+  lastResortPos = { lat: 59.9138688, lon: 10.752245399999993 }; // Last resort position OSLO
+  pos = this.lastResortPos;
   fileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
 
   render() {
@@ -33,8 +36,8 @@ class NewCase extends Component {
           >
             <div className={'form-group'}>
               <label htmlFor="category">Kategori</label>
-              <select className={'form-control'} id={'category'} required>
-                <option selected disabled>
+              <select className={'form-control'} id={'category'} defaultValue={'.null'} required>
+                <option value={'.null'} disabled>
                   Kategori
                 </option>
                 {this.categories.map(e => (
@@ -109,8 +112,8 @@ class NewCase extends Component {
               </div>
             </div>
             <div className={'form-group'}>
-              <select className={'form-control'} id={'last-resort-county'} onChange={this.countyListener} hidden>
-                <option selected disabled>
+              <select defaultValue={'.null'} className={'form-control'} id={'last-resort-county'} onChange={this.countyListener} hidden>
+                <option value={'.null'} disabled>
                   Velg fylke
                 </option>
                 {this.counties.map(e => (
@@ -121,12 +124,13 @@ class NewCase extends Component {
                 ))}
               </select>
               <select
+                defaultValue={'.null'}
                 className={'form-control'}
                 id={'last-resort-municipality'}
                 onChange={this.municipalityListener}
                 hidden
               >
-                <option selected disabled>
+                <option value={'.null'} disabled>
                   Velg kommune
                 </option>
                 {this.municipalities.map(e => (
@@ -389,49 +393,115 @@ class NewCase extends Component {
     console.log('Deleting image file with src = ' + image.src);
   }
 
+  validate(index: number) {
+    switch (index) {
+      case 0:
+        // Validate automatic position
+        return true;
+      case 1:
+        // Validate map marker position
+        return true;
+      case 2:
+        // Validate last resort list selection
+        if (this.list1.selectedIndex !== 0 && this.list2.selectedIndex !== 0) {
+          return true;
+        } else {
+          Notify.danger('Vennligst velg et fylke og en kommune hvor saken finner sted og prøv igjen.');
+          console.warn('County or municipality has not been set.');
+          return false;
+        }
+    }
+  }
+
   submit() {
     console.log('Validating form input.');
     if (this.form.checkValidity() && this.pos) {
       // Basic Built-in HTML5 form validation succeeded. Proceeding to validate using JavaScript.
       let index = this.radioSelector();
-      // DERP!
-      switch (index) {
-        case 0:
-          // Automatic location discovery
-          console.log(
-            'Using automatic location discovery using IP-address and GPS if available to determine position.'
-          );
-          break;
-        case 1:
-          // Map marker
-          console.log('Using a map marker to determine position.');
-          if (this.pos != null) {
-          } else {
-            Notify.warning('Vennligst velg et fylke og en kommune hvor saken finner sted og prøv igjen.');
-          }
-          break;
-        case 2:
-          // Last resort list
-          console.log('Using list selection to determine position.');
-          console.log(this.list1.selectedIndex);
-          if (this.list1.selectedIndex !== 0 && this.list2.selectedIndex !== 0) {
-            this.send();
-          } else {
-            Notify.warning('Vennligst velg et fylke og en kommune hvor saken finner sted og prøv igjen.');
-          }
-          break;
+      let region_id = null;
+      if(this.validate(index)){
+        switch (index) {
+          case 0:
+            // Automatic location discovery
+            console.log(
+              'Using automatic location discovery using IP-address and GPS if available to determine position.'
+            );
+            break;
+          case 1:
+            // Map marker
+            console.log('Using a map marker to determine position.');
+            if (this.pos != null) {
+            } else {
+              Notify.warning('Vennligst trykk på en kommune på kartet hvor saken finner sted og prøv igjen.');
+            }
+            break;
+          case 2:
+            // Last resort list
+            console.log('Using list selection to determine position.');
+            console.log(
+              'Selected options are county = ' +
+              this.counties[this.list1.selectedIndex - 1].name +
+              ' with id = ' +
+              this.counties[this.list1.selectedIndex - 1].county_id +
+              ' and municipality = ' +
+              this.municipalities[this.list2.selectedIndex - 1].name +
+              ' with id = ' +
+              this.municipalities[this.list2.selectedIndex - 1].region_id +
+              '. Custom message is: "' +
+              this.lastResortAddress.value +
+              '".'
+            );
+            this.pos = {
+              lat: this.municipalities[this.list2.selectedIndex - 1].lat,
+              lon: this.municipalities[this.list2.selectedIndex - 1].lon
+            };
+            region_id = this.municipalities[this.list2.selectedIndex - 1].region_id;
+            break;
+        }
+        let user_id = null;
+        let newcase = new Case(
+          null,
+          region_id,
+          user_id,
+          this.categories[this.form.querySelector('#category').selectedIndex - 1].id,
+          null,
+          this.form.querySelector('#title').value,
+          this.form.querySelector('#description').value,
+          null,
+          null,
+          this.pos.lat,
+          this.pos.lon
+        );
+        this.send(newcase);
+      }else{
+        console.log("Secondary validation failed.");
       }
     } else {
       // Basic Built-in HTML5 form validation failed. Cannot send.
       console.log('Failed basic validation.');
-      Notify.warning('Vennligst fyll inn de pålagte feltene i kontaktskjemaet og prøv igjen.');
+      Notify.warning('Vennligst fyll inn de pålagte feltene i skjemaet og prøv igjen.');
     }
   }
 
-  send() {
+  send(obj: Case) {
     console.log('Sending form data to server.');
-    console.log('Position is ' + JSON.stringify(this.pos));
+    console.log('Case is ' + JSON.stringify(obj));
+    caseService
+      .createCase(obj)
+      .then(e => {
+        Notify.success('Din henvendelse er sendt og mottat. Din nyopprettede saks-ID er ' + e.case_id);
+        console.log('Form data transmission success! Case ID: ' + e.case_id);
+        this.props.history.push('/');
+      })
+      .catch((err: Error) => {
+        Notify.danger(
+          'Det oppstod en feil ved sending av saken til oss. Sørg for at alle felter er fyllt ut korrekt. ' +
+            'Hvis problemet vedvarer kan du kontakte oss. \n\nFeilmelding: ' +
+            err.message
+        );
+        console.warn('Error while transmitting form data to server with error message: ' + err.message);
+      });
   }
 }
 
-export default NewCase;
+export default  withRouter(NewCase);
