a4b00be63f3d992f1259203f54c367b7b378a147
ingelig <ingelig@stud.ntnu.no>
2019-01-11 11:23:18 +0100

    Tester med bruk av token

--- server/tests/serverTest.js
+++ server/tests/serverTest.js
@@ -2,6 +2,39 @@
 import { application } from '../src/server';
 import request from 'supertest';
 
+let loginToken=null;
+describe('POST /api/login', async () => {
+  let data = {
+    email:"test@gmail.com",
+    password:"password123"
+  };
+  test('200 status code for POST', function(done) {
+    request(application)
+      .post('/api/login')
+      .send(data)
+      .then(response => {
+        loginToken = response.body.token;
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+if (loginToken!==null) loginToken = "Bearer "+loginToken;
+
+describe('GET /api/users', () => {
+  test('200 status code for GET', done => {
+    request(application)
+      .get('/api/users')
+      .setHeader({Authorization: loginToken})
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+
+});
+
 describe('GET /api/cases', () => {
   test('200 status code for GET', done => {
     request(application)
@@ -11,14 +44,14 @@ describe('GET /api/cases', () => {
         done();
       });
   });
-/*  test('GET method returns a list', done => {
+  test('GET method returns a list', done => {
     request(application)
       .get('/api/cases')
       .then(response => {
-        expect(response.data).toEqual();
+        expect(response.body).toBeInstanceOf(Array);
         done();
       });
-  });*/
+  });
 });
 
 let caseid;
@@ -58,10 +91,27 @@ describe('GET /api/cases/user_cases/{user_id}', () => {
   });
 });
 
-describe('GET /api/cases/region_cases/{county_name}/{region_name}', () => {
-  test('200 status code for GET Trøndelag/Trondheim', done => {
+describe('GET /api/cases/:case_id/status_comments', () => {
+  test('200 status code for GET comments for case_id = 1', done => {
     request(application)
-      .get('/api/cases/region_cases/Trøndelag/Trondheim')
+      .get('/api/cases/1/status_comments')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe('GET /api/cases/:case_id/status_comments', () => {
+  let data = {
+    comment: "Venter på deler",
+    status_id: 2,
+    user_id: 1
+  };
+  test('200 status code for POST comments for case_id = 1', done => {
+    request(application)
+      .put('/api/cases/1/status_comments')
+      .send(data)
       .then(response => {
         expect(response.statusCode).toBe(200);
         done();
@@ -137,6 +187,95 @@ describe('DELETE /api/cases/{case_id}/subscribe', () => {
   test('200 status code for DELETE with case id = 1', done => {
     request(application)
       .delete('/api/cases/1/subscribe')
+      .send(data)
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe('GET /api/cases/subscriptions/:user_id', () => {
+  test('200 status code for GET with user id = 1', done => {
+    request(application)
+      .get('/api/cases/subscriptions/1')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe('GET /api/cases/region_cases/{county_name}/{region_name}', () => {
+  test('200 status code for GET Trøndelag/Trondheim', done => {
+    request(application)
+      .get('/api/cases/region_cases/Trøndelag/Trondheim')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe('GET /api/statuses', () => {
+  test('200 status code for GET', done => {
+    request(application)
+      .get('/api/statuses')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe('POST /api/statuses', () => {
+  let data = {
+    name: "Ugyldig"
+  };
+  test('200 status code for POST', done => {
+    request(application)
+      .post('/api/statuses')
+      .send(data)
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe('GET /api/roles', () => {
+  test('200 status code for GET', done => {
+    request(application)
+      .get('/api/roles')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe('POST /api/roles/', () => {
+  let data = {
+    name: "Test bruker"
+  };
+  test('200 status code', done => {
+    request(application)
+      .put('/api/statuses')
+      .send(data)
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe('PUT /api/roles/:role_id', () => {
+  let data = {
+    name: "Test bruker"
+  };
+  test('200 status code', done => {
+    request(application)
+      .put('/api/statuses')
       .send(data)
       .then(response => {
         expect(response.statusCode).toBe(200);
