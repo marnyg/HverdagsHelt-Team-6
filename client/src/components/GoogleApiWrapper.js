c99e5b1b551f3ef17be56cdc9b6b740a22134c20
Marius <marnyg31.10@gmail.com>
2019-01-13 12:00:28 +0100

    fixed googmap move marken on click

--- client/src/components/GoogleApiWrapper.js
+++ client/src/components/GoogleApiWrapper.js
@@ -3,36 +3,16 @@ import { Component } from 'react-simplified';
 import { GoogleApiWrapper, InfoWindow, Map, Marker, SearchBox } from 'google-maps-react';
 
 export class GoogleMapsContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {}
-    };
-  }
-  onMarkerClick = (props, marker, e) => {
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
-  };
   onClick(t, map, coord) {
-    const latLng = { lat: coord.latLng.lat(), lng: coord.latLng.lng() };
+    const latLng = { lat: coord.latLng.lat(), lon: coord.latLng.lng() };
     this.props.updatePos(latLng);
     this.render();
   }
   render() {
-    const style = {
-      width: '100%',
-      height: '100%'
-    };
     return (
       <Map
         item
         xs={12}
-        style={style}
         google={this.props.google}
         onClick={this.onClick}
         zoom={14}
@@ -42,12 +22,7 @@ export class GoogleMapsContainer extends Component {
         {/* <SearchBox
           controlPosition={google.maps.ControlPosition.TOP_LEFT}
         /> */}
-        <Marker
-          //onClick={this.onMarkerClick}
-          title={'Changing Colors Garage'}
-          position={this.state.clickPos}
-          name={'Changing Colors Garage'}
-        />
+        <Marker position={this.props.userPos} />
       </Map>
     );
   }
