52d75321cc09a6a3f4fb0f9c4dcdf019777b8b8a
Christian Mathias Dalseth <chrismd@stud.ntnu.no>
2019-01-14 14:43:19 +0100

    Varselside

---
+++ client/src/components/CaseSubscriptions.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Component } from 'react-simplified';
+import { NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+
+class CaseSubscription extends Component {
+  subscriptions = [];
+}
