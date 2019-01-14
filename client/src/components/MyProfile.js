45f848ca9f5b0276bc4ecae83b06211293369f89
Marius Nygård <mariunyg@stud.ntnu.no>
2019-01-14 11:50:09 +0100

    added some basic form validation

--- client/src/components/MyProfile.js
+++ client/src/components/MyProfile.js
@@ -8,30 +8,37 @@ import NewCase from './NewCase';
 class MyProfile extends Component<{}, { isEditing: boolean }> {
   state = { isEditing: false };
 
-  user = { name: 'Ola Norman', epost: '123@asd.abc', tlf: '12312312', rolle: 'Mordi' };
+  user = null
   render() {
-    return <div>{this.state.isEditing ? this.getDisplayInfoVersion() : this.getEditFormVersion()}</div>;
+    if (this.user == null) { return <div>404</div> }
+    return <div>{this.state.isEditing ? this.getEditFormVersion() : this.getDisplayInfoVersion()}</div>;
   }
 
-  changToEditVersion(arg: boolean) {
+
+  mounted() {
+    //get user
+    this.user = { name: 'Ola Norman', epost: '123@asd.abc', tlf: '12312312', password: "1" };
+  }
+
+  changToEditVersion(event, arg: boolean) {
+    event.preventDefault()
     this.setState({ isEditing: arg });
-    console.log(this.state);
-    console.log(this.state.isEditing);
   }
   handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     switch (event.target.id) {
       case 'name':
         this.user.name = event.target.value;
-        console.log('changed name to ' + event.target.value);
         break;
       case 'email':
         this.user.epost = event.target.value;
-        console.log('changed email to ' + event.target.value);
         break;
       case 'tlf':
         this.user.tlf = event.target.value;
-        console.log('changed email to ' + event.target.value);
         break;
+      case 'password1':
+        this.user.password = event.target.value;
+        break;
+
     }
     console.log(this.user);
   }
@@ -39,7 +46,7 @@ class MyProfile extends Component<{}, { isEditing: boolean }> {
     return (
       <form id="form-inline">
         <legend>Profil</legend>
-        <label>Naaaaavn: </label>
+        <label>Navn: </label>
         <input
           type="text"
           id="name"
@@ -58,18 +65,45 @@ class MyProfile extends Component<{}, { isEditing: boolean }> {
           className="form-control"
         />
         <label>Tlf</label>
-        <input type="tel" id="tlf" defaultValue={this.user.tlf} onChange={this.handleChange} className="form-control" />
-        <label>Rolle (???????????)</label>
-        <input type="text" defaultValue={this.user.rolle} onChange={this.handleChange} className="form-control" />
-        <button className="btn btn-primary" onClick={() => this.changToEditVersion(true)}>
+        <input type="tel" required id="tlf" defaultValue={this.user.tlf} onChange={this.handleChange} className="form-control" />
+        <label>Nytt Passord</label>
+        <input type="password" required id='password1' onChange={this.handleChange} className="form-control" />
+        <label>Gjenta Passord</label>
+        <input type="password" required id="password2" onChange={this.handleChange} className="form-control" />
+        <button className="btn btn-primary" onClick={this.validateForm}>
           Send Endringer
         </button>
 
-        <button className="btn btn-danger" onClick={() => this.changToEditVersion(true)}>
+        <button className="btn btn-danger" onClick={(e) => this.changToEditVersion(e, false)}>
           Avbryt
         </button>
       </form>
     );
+  }
+  validateForm(event: Event) {
+    event.preventDefault()
+    let form = event.target.parentNode
+    let children = Array.prototype.slice.call(form.children, 0);
+
+    if (this.arePasswordsEqual(children)) {
+
+      console.log("form is valid, this is where you send the data to te server");
+      console.log("you also get the newest profile data from the server");
+
+      this.setState({ isEditing: false })
+      console.log(this.state);
+
+    } else {
+      let passwordInputs = children.filter(e => e.id.includes("password"));
+      passwordInputs.map(e => e.setCustomValidity("Passwords must match"))
+      console.log("her we allert the user that the passwords dont match")
+    }
+
+  }
+
+  arePasswordsEqual(children: HTMLElement) {
+    let passwordInputs = children.filter(e => e.id.includes("password"));
+    return (passwordInputs[1].value === passwordInputs[0].value)
   }
 
   getDisplayInfoVersion() {
@@ -82,14 +116,14 @@ class MyProfile extends Component<{}, { isEditing: boolean }> {
         <p>{this.user.epost}</p>
         <label>Tlf</label>
         <p>{this.user.tlf}</p>
-        <label>Rolle (???????????)</label>
-        <p>{this.user.rolle}</p>
-        <button className="btn btn-primary" onClick={() => this.changToEditVersion(false)}>
+        <button className="btn btn-primary" onClick={(e) => this.changToEditVersion(e, true)}>
           Rediger Profil
         </button>
       </form>
     );
   }
 }
+
+
 
 export default MyProfile;
