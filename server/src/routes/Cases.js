681d3d78fec62bc87d47dc1d1be848a781ae288c
Christian Mathias Dalseth <chrismd@stud.ntnu.no>
2019-01-15 23:27:41 +0100

    asd

--- server/src/routes/Cases.js
+++ server/src/routes/Cases.js
@@ -4,9 +4,6 @@ import { Case, sequelize } from '../models.js';
 import { reqAccessLevel, verifyToken } from '../auth';
 import { Picture } from '../models';
 
-<<<<<<< HEAD
-module.exports = {
-=======
 type Request = express$Request;
 type Response = express$Response;
 
@@ -41,7 +38,6 @@ module.exports = {
       });
   },
 
->>>>>>> master
   createNewCase: function(req: Request, res: Response) {
     reqAccessLevel(req, res, 4, () => true);
     if (!req.files) {
@@ -103,11 +99,7 @@ module.exports = {
               });
         })
         .catch(error => {
-<<<<<<< HEAD
-          return res.status(400).send(error);
-=======
           return res.status(500).send(error);
->>>>>>> master
         });
     }
   },
@@ -123,25 +115,6 @@ module.exports = {
     })
     */
     sequelize
-<<<<<<< HEAD
-      .query(
-        'Select c.case_id, c.title, c.description, c.lat, c.lon, r.name as region_name, s.name as status_name, ' +
-          "CONCAT(u.firstname, ' ', u.lastname) as createdBy, c.createdAt, c.updatedAt " +
-          'FROM Cases c JOIN Regions r ON c.region_id = r.region_id ' +
-          'JOIN Users u ON c.user_id = u.user_id ' +
-          'JOIN Statuses s ON c.status_id = s.status_id ' +
-          'WHERE c.case_id = ?;',
-        { replacements: [req.params.case_id] },
-        { type: sequelize.QueryTypes.SELECT }
-      )
-      .then( async (result) => {
-        let pictures = Picture.findAll({ where: { case_id: req.params.case_id } });
-        let images = await pictures.map(img => img.path);
-        let data = result[0][0];
-        data['images'] = images;
-        return res.send(data);
-      })
-=======
       .query(rawQueryCases + ' WHERE c.case_id = ?;', {
         replacements: [req.params.case_id],
         type: sequelize.QueryTypes.SELECT
@@ -226,6 +199,5 @@ module.exports = {
       .catch(err => {
         return res.status(500).send(err);
       });
->>>>>>> master
   }
 };
