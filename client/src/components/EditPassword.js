7648c7aae77bfc8561fa387b8e890fd6ea6baa39
Marius Nygård <mariunyg@stud.ntnu.no>
2019-01-16 13:56:45 +0100

    asd

--- client/src/components/EditPassword.js
+++ client/src/components/EditPassword.js
@@ -34,21 +34,32 @@ class EditPassword extends Component {
     }
     getEditFormVersion() {
         return (
-            <form id="form-inline">
-
-                <label>Gammelt Passord</label>
-                <input type="password" required id="oldPassword" onChange={this.handleChange} className="form-control" />
-                <label>Nytt Passord</label>
-                <input type="password" required id="password1" onChange={this.handleChange} className="form-control" />
-                <label>Gjenta Passord</label>
-                <input type="password" required id="password2" onChange={this.handleChange} className="form-control" />
-                <button type="submit" className="btn btn-primary" onClick={this.validateForm}>
-                    Send
-                </button>
-                <button className="btn btn-danger" onClick={e => this.props.callback(e, <DisplayProfile callback={this.props.callback} />)}>
-                    Avbryt
-                </button>
-            </form>
+            <div className={'row list-group-item'}>
+                <div className={'row'}>
+                    <div className={'col-sm'}>
+                        Gammelt Passord:
+                            </div>
+                    <div className={'col-lg'}>
+                        <input type="password" required id="oldPassword" onChange={this.handleChange} className="form-control" />
+                    </div>
+                </div>
+                <div className={'row'}>
+                    <div className={'col-sm'}>
+                        Passord:
+                            </div>
+                    <div className={'col-lg'}>
+                        <input type="password" required id="password1" onChange={this.handleChange} className="form-control" />
+                    </div>
+                </div>
+                <div className={'row'}>
+                    <div className={'col-sm'}>
+                        Gjenta passord:
+                            </div>
+                    <div className={'col-lg'}>
+                        <input type="password" required id="password2" onChange={this.handleChange} className="form-control" />
+                    </div>
+                </div>
+            </div>
         );
     }
     validateForm(event: Event) {
