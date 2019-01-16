a7f439ee5a331ade78fe8bbb82bb660c295b31fa
Marius Nygård <mariunyg@stud.ntnu.no>
2019-01-16 13:56:45 +0100

    asd

--- client/src/components/EditPassword.js
+++ client/src/components/EditPassword.js
@@ -71,7 +71,6 @@ class EditPassword extends Component {
                     <div className={'col-lg btn btn-primary'} onClick={this.validateForm}>
                         Lagre
           </div>
-                    {/* asd */}
                 </div>
                 <div
                     className={'col-md btn btn-danger'}
