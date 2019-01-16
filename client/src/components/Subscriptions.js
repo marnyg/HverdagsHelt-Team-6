bbb36fc7b0819f81e5bef116d681f936646fa2dc
Christian Mathias Dalseth <chrismd@stud.ntnu.no>
2019-01-16 12:29:05 +0100

    Varsler funker, bortsett fra selvlagde saker

--- client/src/components/Subscriptions.js
+++ client/src/components/Subscriptions.js
@@ -13,7 +13,7 @@ import Region from '../classes/Region.js';
 class Subscriptions extends Component {
   temp=[];
   subscriptions = [];
-  //user = JSON.parse(localStorage.getItem("users"));
+  user = JSON.parse(localStorage.getItem('user'));
 
   render() {
     if (!this.subscriptions) {
@@ -38,12 +38,9 @@ class Subscriptions extends Component {
   }
 
   mounted() {
-
-    console.log('hei ', this.temp);
-
     let caseSubscriptionService = new CaseSubscriptionService();
     caseSubscriptionService
-    .getAllSubscribedCasesGivenUser(1)
+    .getAllSubscribedCasesGivenUser(this.user.user_id)
     .then((subscriptions: Case[]) => {
       this.subscriptions = subscriptions;
       console.log('tabell: ', subscriptions)
@@ -67,109 +64,5 @@ class Subscriptions extends Component {
     return regionService.getRegion(region_id);
   }
 }
-
-/*
-{this.subscriptions.filter(sub => sub.region_id).map(sub2 => (
-  <div>
-    <h1>{sub2.region_name}</h1>
-    <p>{sub2.des}</p>
-  </div>
-))}
-{this.subscriptions.filter((name, i, subscriptions) => subscriptions.indexOf(name) == i).map(sub => (
-  <div>
-    <h1>{sub.region_name}</h1>
-    <p>{sub.des}</p>
-  </div>
-))}
-<p>Opprettet: {this.subscriptions[i].created_at}, Oppdatert: {this.subscriptions[i].updated_at}</p>
-<p><i>{this.subscriptions[i].description}</i></p>
-{this.subscriptions.map((sub, i) => (
-  <div>
-    <h1>{this.subscriptions[i].region_id}</h1>
-  </div>
-))}
-{this.subscriptions.map((sub, i) => (
-  <div>
-  <p>Opprettet: {this.subscriptions[i].created_at}, Oppdatert: {this.subscriptions[i].updated_at}</p>
-  <p><i>{this.subscriptions[i].description}</i></p>
-  </div>
-))}
-
-/MARIUSKODE
-indexListe.map(index=> {
-  relevantcasse=subscriptions.filter(sub=> sub.region_id===index)
-} )
-
-
-{this.temp.map((e, row) => (
-  e.map((j, col) => (
-    <div>
-      <h1>{this.temp[row][col].region_id}</h1>
-      <p>Opprettet: {this.temp[row][col].created_at}, Oppdatert: {this.temp[row][col].updated_at}</p>
-      <p><b>{this.temp[row][col].title}</b></p>
-      <p><i>{this.temp[row][col].description}</i></p>
-    </div>
-  ))
-))}
-*/
-
-
-
-/*<div>
-  <h1>{this.subscriptions[i].region_id}</h1>
-</div>
-
-{this.temp.map((e, i) => (
-  <div>
-
-  </div>
-))}
-
-{this.temp.map((e, row) => (
-  this.temp.map((j, col) => (
-    <div>
-      <h1>{this.temp[row][col].region_id}</h1>
-      <p>Opprettet: {this.temp[row][col].created_at}, Oppdatert: {this.temp[row][col].updated_at}</p>
-      <p><b>{this.temp[row][col].title}</b></p>
-      <p><i>{this.temp[row][col].description}</i></p>
-    </div>
-  ))
-))}
-
-{this.temp.map((e, i) => (
-  <div>
-    <h1>{this.temp[i][0].region_id}</h1>
-    <p>Opprettet: {this.temp[i][0].created_at}, Oppdatert: {this.temp[i][0].updated_at}</p>
-    <p><b>{this.temp[i][0].title}</b></p>
-    <p><i>{this.temp[i][0].description}</i></p>
-  </div>
-))}
-
-<div>
-  <h1>{this.subscriptions[i].region_id}</h1>
-</div>
-
-  <h1>{this.subscriptions[index].title}</h1>
-  <p>Opprettet: {this.subscriptions[index].created_at}, Oppdatert: {this.subscriptions[index].updated_at}</p>
-  <p><i>{this.subscriptions[index].description}</i></p>
-
-
-
-  FUNKER MED PREDATA:
-
-  <div>
-    {this.temp.map(e => {
-      return <div>
-        <h1>{e[0].region_id}</h1>
-      {e.map(j => (
-        <div>
-          <p><b>{j.title}</b></p>
-          <p>Opprettet: {j.created_at}, Oppdatert: {j.updated_at}</p>
-          <p><i>{j.description}</i></p>
-        </div>
-      ))
-    }</div>})}
-  </div>
-*/
 
 export default withRouter(Subscriptions);
