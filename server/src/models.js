2331c242ac3aa8a0018dff022885191c8eb43100
Jan-Marius <janmariv@stud.ntnu.no>
2019-01-17 08:18:05 +0100

    Adding test data into models.js 5

--- server/src/models.js
+++ server/src/models.js
@@ -191,7 +191,7 @@ Case_subscriptions.belongsTo(Case, { foreignKey: { name: 'case_id', allowNull: f
 
 // Drop tables and create test data when not in production environment
 let production = process.env.NODE_ENV === 'production';
-production = false; // Gjør at databasen er statisk
+production = true; // Gjør at databasen er statisk
 // The sync promise can be used to wait for the database to be ready (for instance in your tests)
 export let sync = sequelize.sync({ force: production ? false : true }).then(
   () => {
