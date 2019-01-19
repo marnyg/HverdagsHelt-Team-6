fac716bd42fbd661d5b53c4e70a5c6a429eefd16
Håvard Ramberg <haavarhr@stud.ntnu.no>
2019-01-19 19:40:29 +0100

    CaseList: La til sletteknapp og abonnerknapp.

--- client/src/components/CaseList.js
+++ client/src/components/CaseList.js
@@ -4,16 +4,22 @@ import ReactDOM from 'react-dom';
 import * as React from 'react';
 import { Component } from 'react-simplified';
 import { NavLink, withRouter } from 'react-router-dom';
-import CaseService from '../services/CaseService';
 import Notify from './Notify';
+import Case from '../classes/Case';
+import CaseService from '../services/CaseService';
+import CaseSubscription from '../classes/CaseSubscription';
+import CaseSubscriptionService from '../services/CaseSubscriptionService';
 
 // Constants used for colouring status fields in table
 
 const statusStyles = [{ color: 'red' }, { color: 'orange' }, { color: 'green' }]; // Constant used for colouring status fields in table.
 const ITEMS_PER_QUERY = 20;
+const STATUS_CAN_DELETE = 1;
+const subscriptionButtonStyles = ['btn btn-info', 'btn btn-outline-info'];
 
 class CaseList extends Component<{ user_id: number, region_id: number }> {
-  cases = [];
+  cases: Case[] = [];
+  subscriptions: CaseSubscription[] = [];
   offset: number = 0;
   fetchButton = null;
 
@@ -33,17 +39,28 @@ class CaseList extends Component<{ user_id: number, region_id: number }> {
               <th scope="col">Eier</th>
               <th scope="col">Dato opprettet</th>
               <th scope="col">Siste oppdatering</th>
+              <th scope="col" />
             </tr>
           </thead>
           <tbody>
             {this.cases.map(c => (
-              <tr key={c.case_id} style={{ cursor: 'pointer' }} onClick={this.onClickTableRow}>
-                <td>{c.title.trim()}</td>
-                <td style={this.getStatusColour(c.status_id)}>{c.status_name}</td>
-                <td>{c.region_name}</td>
-                <td>{c.createdBy}</td>
-                <td>{this.dateFormat(c.createdAt)}</td>
-                <td>{this.dateFormat(c.updatedAt)}</td>
+              <tr key={c.case_id} style={{ cursor: 'pointer' }}>
+                <td onClick={this.onClickTableRow}>{c.title.trim()}</td>
+                <td onClick={this.onClickTableRow} style={this.getStatusColour(c.status_id)}>
+                  {c.status_name}
+                </td>
+                <td onClick={this.onClickTableRow}>{c.region_name}</td>
+                <td onClick={this.onClickTableRow}>{c.createdBy}</td>
+                <td onClick={this.onClickTableRow}>{this.dateFormat(c.createdAt)}</td>
+                <td onClick={this.onClickTableRow}>{this.dateFormat(c.updatedAt)}</td>
+                <td>
+                  <button className={'btn btn-danger'} hidden={!this.canDelete(c)} onClick={this.onClickDeleteButton}>
+                    Slett
+                  </button>
+                  <button className={this.getSubscriptionButtonStyles(c)} onClick={this.onClickSubscribeButton}>
+                    Abonner
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
@@ -53,7 +70,7 @@ class CaseList extends Component<{ user_id: number, region_id: number }> {
             this.fetchButton = e;
           }}
           className={'btn btn-secondary'}
-          onClick={this.onClickButton}
+          onClick={this.fetchCases}
         >
           Last mer
         </button>
@@ -66,17 +83,18 @@ class CaseList extends Component<{ user_id: number, region_id: number }> {
 
   mounted() {
     console.log('user_id: ' + this.props.user_id + ', region_id: ' + this.props.region_id);
-    this.fetchData();
+    this.fetchCases();
+    this.fetchSubscriptions();
   }
 
-  fetchData() {
+  fetchCases() {
     let cas = new CaseService();
     if (this.props.user_id) {
       // Set up table for cases on per user basis
       cas
         .getAllCasesGivenUser(this.props.user_id)
         .then(cases => {
-          this.cases = cases.concat(cases);
+          this.cases.push.apply(this.cases, cases);
           this.offset += cases.length;
           if (this.fetchButton && cases.length < ITEMS_PER_QUERY) {
             this.fetchButton.hidden = true;
@@ -87,14 +105,16 @@ class CaseList extends Component<{ user_id: number, region_id: number }> {
             'Det oppstod en feil under henting av dine saker. Hvis feilen vedvarer kontakt oss. \n\nFeilmelding: ' +
               err.message
           );
-          console.log('Error when fetching cases for user with id ' + this.props.user_id + '.\nError message is: ' + err.message);
+          console.log(
+            'Error when fetching cases for user with id ' + this.props.user_id + '.\nError message is: ' + err.message
+          );
         });
     } else if (this.props.region_id) {
       // Set up table for cases on per municipality/region basis
       cas
         .getAllCasesGivenRegionId(this.props.region_id)
         .then(cases => {
-          this.cases = cases.concat(cases);
+          this.cases.push.apply(this.cases, cases);
           this.offset += cases.length;
           if (this.fetchButton && cases.length < ITEMS_PER_QUERY) {
             this.fetchButton.hidden = true;
@@ -105,7 +125,12 @@ class CaseList extends Component<{ user_id: number, region_id: number }> {
             'Det oppstod en feil under henting av kommunens saker. Hvis feilen vedvarer kontakt oss. \n\nFeilmelding: ' +
               err.message
           );
-          console.log('Error when fetching cases for region with id ' + this.props.region_id + '.\nError message is: ' + err.message);
+          console.log(
+            'Error when fetching cases for region with id ' +
+              this.props.region_id +
+              '.\nError message is: ' +
+              err.message
+          );
         });
     } else {
       console.warn("Didn't find user_id or region_id");
@@ -114,7 +139,59 @@ class CaseList extends Component<{ user_id: number, region_id: number }> {
       );
     }
   }
-  
+
+  fetchSubscriptions() {
+    let sub = new CaseSubscriptionService();
+    let user_id = this.getUserId();
+    sub
+      .getAllCaseSubscriptions(user_id)
+      .then(e => {
+        this.subscriptions = e;
+      })
+      .then(() => console.log('this.subscriptions: ', this.subscriptions))
+      .catch((err: Error) => {
+        console.log('Could not fetch subscriptions for user with id: ' + user_id);
+        Notify.danger(
+          'Kunne ikke hente abonnement. Hvis problemet vedvarer kontakt oss. \n\nFeilmelding: ' + err.message
+        );
+      });
+  }
+
+  getUserId() {
+    let userString = localStorage.getItem('user');
+    if (userString) {
+      return JSON.parse(userString).user_id;
+    } else {
+      console.log('User is not logged in!');
+      Notify.danger('Du må logge inn for å bruke denne tjenesten!');
+      return -1;
+    }
+  }
+
+  isOwner(c: Case) {
+    return this.getUserId() === c.user_id;
+  }
+
+  isSubscribed(c: Case) {
+    return this.subscriptions.some(e => e.case_id === c.case_id);
+  }
+
+  getSubscriptionButtonStyles(c: Case) {
+    if (this.isSubscribed(c)) {
+      return subscriptionButtonStyles[1];
+    } else {
+      return subscriptionButtonStyles[0];
+    }
+  }
+
+  canDelete(c: Case) {
+    if (c.status_id === STATUS_CAN_DELETE) {
+      return this.isOwner(c);
+    } else {
+      return false;
+    }
+  }
+
   getStatusColour(status_id: number) {
     return statusStyles[status_id + 1];
   }
@@ -130,7 +207,64 @@ class CaseList extends Component<{ user_id: number, region_id: number }> {
     }
   }
 
-  onClickButton(event: SyntheticInputEvent<HTMLButtonElement>) {}
+  onClickDeleteButton(event: SyntheticInputEvent<HTMLButtonElement>) {
+    console.log('Trykket SLETT!');
+    let cas = new CaseService();
+    let td = event.target.parentElement;
+    if (td && td.parentElement && td.parentElement instanceof HTMLTableRowElement) {
+      let case_id = this.cases[td.parentElement.rowIndex - 1].case_id;
+      console.log('Requesting to delete case with id: ' + case_id);
+      cas
+        .deleteCase(case_id)
+        .then(e => {
+          console.log('Delete returned: ', e);
+          this.cases.filter(e => e.case_id !== case_id);
+        })
+        .catch((err: Error) => {
+          console.log('Could not delete case with id ' + case_id + ': ', err);
+          Notify.danger('Kunne ikk slette sak. \n\nFeilmelding: ' + err.message);
+        });
+    } else {
+      console.log('Did not find case_id to delete.');
+    }
+  }
+
+  onClickSubscribeButton(event: SyntheticInputEvent<HTMLButtonElement>) {
+    console.log('Clicked subscribe button!');
+    let sub = new CaseSubscriptionService();
+    let td = event.target.parentElement;
+    if (td && td.parentElement && td.parentElement instanceof HTMLTableRowElement) {
+      let c = this.cases[td.parentElement.rowIndex - 1];
+      if (this.isSubscribed(c)) {
+        // Unsubscribe the user from this case
+        sub
+          .deleteCaseSubscription(c.case_id, this.getUserId())
+          .then(e => {
+            this.subscriptions.filter(f => f.case_id !== c.case_id);
+            console.log("Unsubscribed, returned: ",e);
+          })
+          .catch((err: Error) => {
+            console.log('Could not unsubscribe user from case with id ' + c.case_id);
+            Notify.warning('Det oppstod en feil ved sletting av abonnement på saken. \n\nFeilmelding: ' + err.message);
+          });
+      } else {
+        // Subscribe this case to user
+        let s = new CaseSubscription(this.getUserId(), c.case_id, false, true);
+        sub
+          .createCaseSubscription(s)
+          .then(e => {
+            console.log("Subscribed, returned: ", e);
+            this.subscriptions.push(e);
+          })
+          .catch((err: Error) => {
+            console.log('Could not unsubscribe user from case with id ' + c.case_id);
+            Notify.warning('Det oppstod en feil ved sletting av abonnement på saken. \n\nFeilmelding: ' + err.message);
+          });
+      }
+    } else {
+      console.log('Did not find case_id to alter subscription.');
+    }
+  }
 
   dateFormat(date: string) {
     if (date) {
