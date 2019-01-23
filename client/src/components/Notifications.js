5a2701b142f8cfe9e581014ca3db1f65db568753
Martin Andreas Moan <martimoa@stud.ntnu.no>
2019-01-23 15:02:53 +0100

    Sockets

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -17,79 +17,79 @@ import CaseItem from './CaseItem.js';
 import { faListUl, faTh } from '@fortawesome/free-solid-svg-icons/index';
 
 class Notifications extends Component {
-  grid = true;
-  cases = [];
-  user = JSON.parse(localStorage.getItem('user'));
+    grid = true;
+    cases = [];
+    user = JSON.parse(localStorage.getItem('user'));
 
-  render() {
-    if (this.cases.length === 0) {
-      return <div>
-        <h1>Vi vil holde deg oppdatert!</h1>
-        <p>Alle våre ansatte gjør sitt yttertste for å løse dine saker.
-        Så fort det oppstår endringer i din sak, eller saker du har abonnert på, vil disse dukke opp på denne siden.
-        Vennligst kom tilbake senere.</p>
-        <p><strong>Vi i Hverdagshelter ønsker deg fin dag videre!</strong></p>
-      </div>
-    }
+    render() {
+        if (this.cases.length === 0) {
+            return <div>
+                <h1>Vi vil holde deg oppdatert!</h1>
+                <p>Alle våre ansatte gjør sitt yttertste for å løse dine saker.
+                    Så fort det oppstår endringer i din sak, eller saker du har abonnert på, vil disse dukke opp på denne siden.
+                    Vennligst kom tilbake senere.</p>
+                <p><strong>Vi i Hverdagshelter ønsker deg fin dag videre!</strong></p>
+            </div>
+        }
 
-    return(
-        <div>
-            <NavLink to={'/new-case'} className={'btn btn-primary btn-lg w-100 mb-3 megabutton'}>Registrer sak</NavLink>
+        return(
             <div>
-                <div className="d-none d-sm-block">
-                    <div className="btn-toolbar my-3 mx-2" role="toolbar">
-                        <div className="btn-group mr-2" role="group">
-                            <button
-                                type="button"
-                                className={this.grid ? 'btn btn-secondary' : 'btn btn-secondary'}
-                                onClick={() => (this.grid = true)}
-                            >
-                                <FontAwesomeIcon icon={faTh} /> Grid
-                            </button>
-                            <button type="button" className="btn btn-secondary" onClick={() => (this.grid = false)}>
-                                <FontAwesomeIcon icon={faListUl} /> List
-                            </button>
+                <NavLink to={'/new-case'} className={'btn btn-primary btn-lg w-100 mb-3 megabutton'}>Registrer sak</NavLink>
+                <div>
+                    <div className="d-none d-sm-block">
+                        <div className="btn-toolbar my-3 mx-2" role="toolbar">
+                            <div className="btn-group mr-2" role="group">
+                                <button
+                                    type="button"
+                                    className={this.grid ? 'btn btn-secondary' : 'btn btn-secondary'}
+                                    onClick={() => (this.grid = true)}
+                                >
+                                    <FontAwesomeIcon icon={faTh} /> Grid
+                                </button>
+                                <button type="button" className="btn btn-secondary" onClick={() => (this.grid = false)}>
+                                    <FontAwesomeIcon icon={faListUl} /> List
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div>
-                    {this.grid ? (
-                        <div className="content">
-                            {this.cases.map(e => (
-                                <CaseItem case={e} key={e.case_id} grid={this.grid} user={this.user}/>
-                            ))}
-                        </div>
-                    ) : (
-                        this.cases.map(e => <CaseItem case={e} key={e.case_id} grid={this.grid} user={this.user}/>)
-                    )}
+                    <div>
+                        {this.grid ? (
+                            <div className="content">
+                                {this.cases.map(e => (
+                                    <CaseItem case={e} key={e.case_id} grid={this.grid} user={this.user}/>
+                                ))}
+                            </div>
+                        ) : (
+                            this.cases.map(e => <CaseItem case={e} key={e.case_id} grid={this.grid} user={this.user}/>)
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-  }
-
-  mounted() {
-    let caseSubscriptionService = new CaseSubscriptionService();
-    caseSubscriptionService
-    .getAllOutdatedCaseSubscriptions(this.user.user_id)
-    .then((cases: Case[]) => {
-      for (var i = 0; i < cases.length; i++) {
-        cases[i].subscribed = true;
-      }
-      this.cases = cases;
-      console.log('tabell: ', cases)
-    })
-    .catch((error: Error) => console.error(error));
-  }
-
-  dateFormat(date: string) {
-    if (date) {
-      let a = date.split('.')[0].replace('T', ' ');
-      return a.substr(0, a.length - 3);
-    } else {
-      return 'Fant ikke dato.';
+        );
     }
-  }
+
+    mounted() {
+        let caseSubscriptionService = new CaseSubscriptionService();
+        caseSubscriptionService
+            .getAllOutdatedCaseSubscriptions(this.user.user_id)
+            .then((cases: Case[]) => {
+                for (var i = 0; i < cases.length; i++) {
+                    cases[i].subscribed = true;
+                }
+                this.cases = cases;
+                console.log('tabell: ', cases);
+            })
+            .catch((error: Error) => console.error(error));
+    }
+
+    dateFormat(date: string) {
+        if (date) {
+            let a = date.split('.')[0].replace('T', ' ');
+            return a.substr(0, a.length - 3);
+        } else {
+            return 'Fant ikke dato.';
+        }
+    }
 }
 
 export default Notifications;
