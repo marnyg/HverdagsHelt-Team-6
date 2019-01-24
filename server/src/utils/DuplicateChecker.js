5d0d9f3ad04a56552d0e23615e12fae89adc7095
magnewor <magnewor@stud.ntnu.no>
2019-01-24 15:24:28 +0100

    DuplicateChecker modules JSDocs added

--- server/src/utils/DuplicateChecker.js
+++ server/src/utils/DuplicateChecker.js
@@ -5,21 +5,31 @@ import Sequelize from 'sequelize';
 const Op = Sequelize.Op;
 
 module.exports = {
-  // returns true if it's a duplicate
+  /**
+   * Checks if there is a case within 50 meters, with the same category. Accepts it if default region position is used
+   * @param lat Latitude
+   * @param lon Longitude
+   * @param category_id Category_id
+   * @param region_id Region_id
+   * @returns {Promise<boolean>}
+   */
   duplicateCheck: function(lat: number, lon: number, category_id: number, region_id: number) {
     return new Promise(resolve => {
-        Region.findOne({ where: { region_id: region_id } }).then(async region => {
-          if (region.lat == lat && region.lon == lon) resolve(false);
-          else {
-            let cases = await Case.findAll({ where: { region_id: region_id, category_id: category_id, status_id: { [Op.ne]: 3 } }, attributes: ['lat', 'lon'] });
-            console.log(cases);
-            resolve(
-              cases.some(the_case => {
-                return isDuplicate(lat, lon, the_case.lat, the_case.lon);
-              })
-            );
-          }
-        });
+      Region.findOne({ where: { region_id: region_id } }).then(async region => {
+        if (region.lat == lat && region.lon == lon) resolve(false);
+        else {
+          let cases = await Case.findAll({
+            where: { region_id: region_id, category_id: category_id, status_id: { [Op.ne]: 3 } },
+            attributes: ['lat', 'lon']
+          });
+          console.log(cases);
+          resolve(
+            cases.some(the_case => {
+              return isDuplicate(lat, lon, the_case.lat, the_case.lon);
+            })
+          );
+        }
+      });
     });
   }
 };
@@ -41,5 +51,5 @@ function isDuplicate(lat1: number, lon1: number, lat2: number, lon2: number) {
 }
 
 function toRad(grad: number) {
-    return (grad * Math.PI) / 180;
+  return (grad * Math.PI) / 180;
 }
