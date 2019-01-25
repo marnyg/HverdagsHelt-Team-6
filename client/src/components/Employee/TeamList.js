64ae56e831d7c7d7cff9b394233fed5f4a0e527a
Martin Andreas Moan <martimoa@stud.ntnu.no>
2019-01-25 13:51:14 +0100

    Fewer bugs

---
+++ client/src/components/Employee/TeamList.js
@@ -0,0 +1,45 @@
+//@flow
+import * as React from 'react';
+import { Component } from 'react-simplified';
+import {faTrashAlt} from "@fortawesome/free-solid-svg-icons/index";
+
+class TeamList extends Component{
+
+    render() {
+        return(
+            <div className={'container'}>
+                <table className={'table table-hover table-striped'}>
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Fornavn</th>
+                            <th scope="col">Etternavn</th>
+                            <th scope="col">Epost</th>
+                            <th scope="col">Telefon</th>
+                            <th scope="col">Tilgangsnivå</th>
+                            <th scope="col">Slett</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {this.props.users.map(user => {
+                        return(
+                            <tr key={user.user_id} style={{ cursor: 'pointer' }}>
+                                <td onClick={(event) => this.props.onUserSelected(user)}>{user.user_id}</td>
+                                <td onClick={(event) => this.props.onUserSelected(user)}>{user.email}</td>
+                                <td onClick={(event) => this.props.onUserSelected(user)}>{this.getRole(user.role_id)}</td>
+                                <td onClick={(event) => this.props.deleteUser(user)}>
+                                    <button className={'btn btn-danger'} type="button" data-toggle="modal"
+                                            data-target="#verify-modal">
+                                        <FontAwesomeIcon icon={faTrashAlt}/>
+                                    </button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+export default TeamList;
